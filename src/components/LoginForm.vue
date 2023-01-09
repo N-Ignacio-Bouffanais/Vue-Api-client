aa28b6be86e2a17f696709e12657b04ea187d83f
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-01-09 00:45:42 -0300

    v-model email password ref vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,12 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, watchEffect } from "vue"
+
+let email = ref("")
+let password = ref("")
+
+watchEffect(()=>{
+    console.log(email.value)
+    console.log(password.value)
+})
+
+</script>
 <template>
-    <form action="submit" class="form-container">
+    <form  class="form-container">
         <p class="input-form">
-            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="email" name="email" id="email" placeholder="Enter your email" v-model="email">
         </p>
         <p class="input-form">
-            <input type="password" name="password" id="password" placeholder="******">
+            <input type="password" name="password" id="password" placeholder="******" v-model="password">
         </p>
+        <button>Save</button>
     </form>
 </template>
 <style lang="scss">
